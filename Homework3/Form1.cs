3cbb607a2781fb1e0edbd6ad52233e79ce2e882e
GlebALeshlov <dimadima0805198585@gmail.com>
2022-12-16 20:33:51 +0300

    Initial commit

---
+++ Homework3/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Top= 0;
+            button1.Width= 200;
+            button1.Height= 150;
+        }
+    }
+}
